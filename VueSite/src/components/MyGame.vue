32489fd8c3cfb4280043d5cb9cb7ce998e974542
Bruendermiester <jonbruenderman@hotmail.com>
2018-03-10 13:28:40 -0800

    Spawning dots working

--- VueSite/src/components/MyGame.vue
+++ VueSite/src/components/MyGame.vue
@@ -5,7 +5,7 @@
           <div class="column" v-for="row in board.rows">
               <div class="row" v-for="spot in row.spots">
                   <div class="tile">
-                      <div class="dot" :style="[spot.isCovered ? {backgroundColor: spot.color} : '' ]" @mousedown="selectDot(spot)">
+                      <div class="dot" :style="[spot.isCovered ? {backgroundColor: spot.color, cursor: 'pointer'} : '' , spot.isSelected ? {border: '1px solid #FFF'} : '']" @mousedown="selectDot(spot)">
 
                       </div>
                   </div>
@@ -20,18 +20,30 @@ export default {
   name: 'Game',
   data () {
     return {
-        board : this.createBoard(),
-        isSelected: {
+        boardSize: [10,10], 
+        board: {},
+        dotSelected: {
             value: false,
             cordinates: {}
-        }
+        },
+        boardIndexList: {}
     }
   },
+  created: function() {
+      this.boardIndexList = this.generateIndexList();
+      this.board = this.createBoard();
+      this.spawnDots(3);
+  },
   methods: {
+      generateIndexList() {
+          //Generate a list(0-99) based on boardsize
+          return Array.apply(null, {length: (this.boardSize[0] * this.boardSize[1])}).map(Number.call, Number);
+      },
       createBoard: function() {
           var board = {
               rows: []
           }
+          var index = 0;
           for(var i = 0; i <= 9; i++) {
               var row = {};
               row.spots = [];
@@ -41,45 +53,73 @@ export default {
                         x: j,
                         y: i
                       },
+                      index: index,
                       isSelected: false,
                       isCovered: false,
                       color: 'red'
                   };
-                  
+                  index++;
                   row.spots.push(spot);
               }
             board.rows.push(row);
           }
           console.log(board)
-          board.rows[1].spots[1].isSelected = true;
-          board.rows[1].spots[1].isCovered = true;
         return board;
       },
       updateBoard: function() {
-
+          this.spawnDots(3);
       },
       selectDot: function(spot) {
-          if(!this.isSelected.value && spot.isCovered) {
-              this.isSelected.value = true;
-              this.isSelected.cordinates = spot.cordinates;
+          if(!this.dotSelected.value && spot.isCovered) {
+              this.dotSelected.value = true;
+              this.dotSelected.cordinates = spot.cordinates;
               spot.isSelected = true;
           }
-          else if(spot.cordinates === this.isSelected.cordinates) {
-              console.log("yes");
+          else if(spot.cordinates === this.dotSelected.cordinates) {
+              this.dotSelected.value = false;
+              this.dotSelected.cordinates = {};
+              spot.isSelected = false;              
           }
           else {
-              console.log(spot.cordinates, this.isSelected)
+              this.moveDot();
           }
 
       },
       moveDot: function() {
+          
 
       },
       checkConnectFive: function() {
 
       },
-      spawnDots: function() {
+      spawnDots: function(numberToSpawn) {
+          if(this.boardIndexList.length > 3) {
+            for(var x = 0; x < numberToSpawn; x++) {
+                var index = this.getRandomNumber(0, this.boardIndexList.length);
+                var position = this.boardIndexList.splice(index, 1)[0];
+                for(var y = 0; y < this.board.rows.length; y++) {
+                    for(var z = 0; z < this.board.rows[y].spots.length; z++) {    
+                        if(position === this.board.rows[y].spots[z].index) {
+                            this.board.rows[y].spots[z].isCovered = true;
+                            this.board.rows[y].spots[z].color = this.pickColor();
+                        }
+                    }
+                }
+            }
+          }
+          else {
+              alert("You Lose!");
+          }
           
+      },
+      pickColor: function() {
+          var colorList = ['red', 'green', 'blue', 'yellow', 'purple'];
+          var random = this.getRandomNumber(0, 5);
+          return colorList[random];
+
+      },
+      getRandomNumber: function(min, max) {
+          return Math.floor(Math.random() * (max - min) + min);
       }
       
   }
