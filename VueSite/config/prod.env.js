823b0e923053e9cc596562d62ead2fada4a7918d
Jonathan <jonathan.bruenderman@infospace.com>
2019-02-26 15:37:13 -0800

    update

---
+++ VueSite/config/prod.env.js
@@ -0,0 +1,10 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  API_KEY: '"AIzaSyCJmAeYa0DwJKnqz0FF5X79M3LyHtMgC2s"',
+  AUTH_DOMAIN: '"personalvuewebsite.firebaseapp.com"',
+  DATABASE_URL: '"https://personalvuewebsite.firebaseio.com"',
+  PROJECT_ID: '"personalvuewebsite"',
+  STORAGE_BUCKET: '""',
+  MESSAGING_SENDER_ID:'"1052675234629"'
+}
